7f351c37e4277c8bfa58bea2509c147aa50c0328
trankhoaminh <tranminhbvhp@gmail.com>
2024-05-02 09:53:44 +0700

    FIX (app controller): public route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Request } from '@nestjs/common';
+import { Public } from 'src/auth/auth.decorators';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Public()
   getHello(): string {
     return this.appService.getHello();
   }
