46595c13e2a9bdc41654a4bf166239a8ff3ca125
Minh <tranminhbvhp@gmail.com>
2024-05-03 00:38:29 +0700

    FIX (jwt strategy): throw error if validate failed

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -2,6 +2,7 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, ExtractJwt } from 'passport-jwt';
 import { UsersService } from 'src/users/users.service';
+import { THIS_FEATURE_NEED_LOGIN } from 'src/utils/messageConstants';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -20,15 +21,22 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
    */
 
   async validate(payload: any) {
-    //check id existed
     const user = await this.usersService.getUserById(payload?.sub);
 
+    //check id existed
+    if (!user) {
+      throw new UnauthorizedException(THIS_FEATURE_NEED_LOGIN);
+    }
+
     //check email match id
-    if (user?.email === payload.email) {
-      return { userId: payload.sub, email: payload.email, role: payload.role };
+    if (user?.email !== payload.email) {
+      throw new UnauthorizedException(THIS_FEATURE_NEED_LOGIN);
     }
 
     //check jwt token expired
+
     //check refresh token expired
+
+    return { userId: payload.sub, email: payload.email, role: payload.role };
   }
 }
